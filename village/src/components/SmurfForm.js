80e1072b1cc45331b35e43a178c34b498235b7f9
anthony <anthony24.kwhomes@gmail.com>
2019-04-12 10:36:15 -0700

    Trying to fix an error before styling.

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
+
 
 class SmurfForm extends Component {
   constructor(props) {
@@ -14,12 +17,25 @@ class SmurfForm extends Component {
     event.preventDefault();
     // add code to create the smurf using the api
 
-    this.setState({
-      name: '',
-      age: '',
-      height: ''
-    });
-  }
+    axios
+      .post(`${this.props.baseUrl}/smurfs`, this.state)
+      .then(res => {
+        this.setState({
+          name: '',
+          age: '',
+          height: ''
+        });
+
+      {this.props.addSmurfs() }
+      this.props.history.push('/');
+    })
+
+    .catch (err => 
+      console.log(err))
+
+    }
+
+    
 
   handleInputChange = e => {
     this.setState({ [e.target.name]: e.target.value });
@@ -47,7 +63,7 @@ class SmurfForm extends Component {
             value={this.state.height}
             name="height"
           />
-          <button type="submit">Add to the village</button>
+          <button type="submit" className='button'>Add to the village</button>
         </form>
       </div>
     );
